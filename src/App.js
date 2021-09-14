2d8247967ad5603b1220c45158960c97bcde2def
ZounaikhKid <zounaikhajw@gmail.com>
2021-09-14 11:43:07 +0100

    checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+import photo from './house.jpg';
+import video from './video.mp4';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title red">zounaikha's houses</h1>
+    <br />
+    <img  src={photo} alt="" />
+    <br />
+    <img src='/home.jpg' alt="home"/>
+  </div>
+  <video width={320} height={240} controls>
+    <source src={video} type="video/mp4" />
+  </video>
     </div>
   );
 }
